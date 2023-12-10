5622c60876d9dd56a75492e61a9aaaeac61afdc4
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-12-10 18:56:32 +0200

    Add React state management to enable dynamic navigation between steps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,16 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1)
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1)
+  }
+
+  function handleNext() {
+    if (step < 3 ) setStep(step + 1)
+  }
+
   return (
     <div className="steps">
      <div className='numbers' >
@@ -17,8 +28,8 @@ function App() {
      <p className="message">Step {step}: {messages[step -1]}</p>
 
     <div className="buttons">
-      <button style={{  backgroundColor: '#7950f2', color: "#fff" }}>Previous</button>
-      <button style={{  backgroundColor: '#7950f2', color: "#fff" }}>Next</button>
+      <button style={{  backgroundColor: '#7950f2', color: "#fff" }} onClick={handlePrevious}>Previous</button>
+      <button style={{  backgroundColor: '#7950f2', color: "#fff" }} onClick={handleNext}>Next</button>
     </div>
     </div>
   );
